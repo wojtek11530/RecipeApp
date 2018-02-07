39a3fc2f039015f3aa10b2b20b23f7d18b087edb
Wojtek <wojtek19962a32@gmail.com>
2018-02-07 15:58:01 +0100

    Initial commit

---
+++ src/main/java/com/example/RecipeAssignment/RecipeAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.example.RecipeAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAssignmentApplication.class, args);
+	}
+}
